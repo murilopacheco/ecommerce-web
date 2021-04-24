d0b0bd1a374408693a003b65ecb6b8afdc62c4e1
murilopacehco <muriloredes@gmail.com>
2021-04-23 21:47:32 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {ClienteModule} from './cliente/cliente.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    ClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/model/cliente-dto.ts
@@ -0,0 +1,8 @@
+export class ClienteDto {
+  id: number;
+  nome: string;
+  cpf: string;
+  primeiroNome: string;
+  dataNascimento: Date;
+  versao: number;
+}
